fce3077b66b8439ab97f236ccec49a3273a38408
Danil Listopad <vip313z@ukr.net>
2021-04-06 11:02:18 +0300

    Сделал текст при нажатии на картинку

---
+++ app.js
@@ -0,0 +1,33 @@
+let text = document.querySelector(".list__text");
+        console.log(text)
+        function showFonts() {
+            text.innerHTML = '<a href="#">' + `textgenerator.ru - Створюйте тексти з красивими шрифтами і копіюйте їх онлайн` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' + `fonts.google.com - бібліотека шрифтів від Google`+ '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`fontsquirrel.com - безкоштовні шрифти` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`fonts-online.ru - Велика колекція різноманітних шрифтів` + '</a>' 
+        }
+        function showEditors() {
+            text.innerHTML = '<a href="#">' + `Visual Studio Code — редактор вихідного коду, позиціонується як «легкий» редактор коду для кроссплатформенной розробки веб-і хмарних додатків` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' + `Brackets — вільний текстовий редактор для веб-розробників. Brackets орієнтований на роботу з HTML, CSS і JavaScript.`+ '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`Atom — безкоштовний текстовий редактор з підтримкою плагінів, написаних на Node.js, і вбудованих під керуванням Git.` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`Eclipse — вільна інтегроване середовище розробки модульних кроссплатформенних додатків.` + '</a>'
+        }
+
+        function showEducation() {
+            text.innerHTML = '<a href="#">' + `Codepen- інтернет-спільнота, створене для тестування і показу фрагментів на основі HTML, CSS і JavaScript.` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' + `W3Schools - це освітній веб-сайт для навчання програмуванню в мережі інтернет.`+ '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`Codewars - освітнє співтовариство з комп'ютерного програмування.` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`learn.javascript.ru - Сучасний підручник JavaScript` + '</a>'
+        }
+
+        function showBlogs() {
+            text.innerHTML = '<a href="#">' + `CSS-Tricks — офіційний акаунт ресурсу CSS-Tricks.com, що об'єднує веб-дизайнерів. (Twitter)` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' + `CSS {IRL} — поради, лайфхак, хитрості і посилання на тему CSS. (Twitter)`+ '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`htmlacademy.ru/blog - Багато корисного матеріалу від відмінного ресурсу дистанційного навчання.` + '</a>'
+        }
+        
+        function showHelp() {
+            text.innerHTML = '<a href="#">' + `color.hailpixel.com - незвичайний підбір кольорової палітри` + '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' + `www.freepik.com - безкоштовні картинки і іконки`+ '</a>' + '<br>' + '<br>' +
+                             '<a href="#">' +`github.com/heyalexej/awesome-images - величезний список ресурсів з безкоштовними фотографіями` + '</a>'
+        }
