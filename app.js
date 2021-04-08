823f44797f4d4657960b8cc3b32d97f26140ce5b
Danil Listopad <vip313z@ukr.net>
2021-04-08 22:35:53 +0300

    добавил заготовку кода для кнопки

--- app.js
+++ app.js
@@ -30,3 +30,68 @@ let text = document.querySelector(".list__text");
                              '<a href="https://www.freepik.com/" target="_blank">' + `www.freepik.com - безкоштовні картинки і іконки`+ '</a>' + '<br>' + '<br>' +
                              '<a href="https://adamovna.ru/300saitov-for-designer/" target="_blank">' +`adamovna.ru - 300 корисних сайтів в допомогу дизайнерові` + '</a>'
         }
+        function getRandomInRange(min, max) {
+            let result = Math.floor(Math.random() * (max - min + 1)) + min;
+            switch (result) {
+                case 1:
+                  alert( '1' );
+                  break;
+                case 2:
+                  alert( '2' );
+                  break;
+                case 3:
+                  alert( '3' );
+                  break;
+                case 4:
+                  alert( '4' );
+                  break;
+                case 5:
+                  alert( '5' );
+                  break;
+                case 6:
+                  alert( '6' );
+                  break;
+                case 7:
+                  alert( '7' );
+                  break;
+                case 8:
+                  alert( '8' );
+                  break;
+                case 9:
+                  alert( '9' );
+                  break;
+                case 10:
+                  alert( '10' );
+                  break;
+                case 11:
+                  alert( '11' );
+                  break;
+                case 12:
+                  alert( '12' );
+                  break;
+                case 13:
+                  alert( '13' );
+                  break;
+                case 14:
+                  alert( '14' );
+                  break;
+                case 15:
+                  alert( '15' );
+                  break;
+                case 16:
+                  alert( '16' );
+                  break;
+                case 17:
+                  alert( '17' );
+                  break;
+                case 18:
+                  alert( '18' );
+                  break;
+                case 19:
+                  alert( '19' );
+                  break;
+                case 20:
+                  alert( '20' );
+                  break;
+            }
+        }
